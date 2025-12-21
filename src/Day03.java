15d601bd23b0d78f724526a8d628a9dd511652f8
Cesar D. Garcia <cesardariogar@gmail.com>
2025-12-21 00:10:33 -0700

    complete Day03 - Part II

--- src/Day03.java
+++ src/Day03.java
@@ -2,7 +2,7 @@ public static final Path FILE_PATH = Path.of("resources/day03/lobby.txt");
 //public static final Path FILE_PATH = Path.of("resources/day03/lobby_small.txt");
 
 /***
- *
+ * Part I:
  * find the largest possible joltage each bank can produce. In the above example:
  *
  *     In 987654321111111, you can make the largest joltage possible, 98, by turning on the first two batteries.
@@ -14,30 +14,37 @@ public static final Path FILE_PATH = Path.of("resources/day03/lobby.txt");
  *     For lobby.txt -> result is: 16946
  *
  * The total output joltage is the sum of the maximum joltage from each bank, so in this example, the total output joltage is 98 + 89 + 78 + 92 = 357.
+ * Find the maximum joltage possible from each bank
  *
- * There are many batteries in front of you. Find the maximum joltage possible from each bank; what is the total output joltage?
- *
+ * Part II:
+ * Now, you need to make the largest joltage by turning on exactly twelve batteries within each bank.
+ * The joltage output for the bank is still the number formed by the digits of the batteries you've turned on;
+ * the only difference is that now there will be 12 digits in each bank's joltage output instead of two.
  */
 
 void main() throws IOException {
     try (var lines = Files.lines(FILE_PATH)) {
         long totalSum = lines
                 .filter(Predicate.not(String::isBlank))
-                .mapToLong(this::maxJoltage)
+                .mapToLong(line -> maxJoltage(line, 12))
                 .sum();
 
         System.out.println("total is: " + totalSum);
     }
 }
 
-private long maxJoltage(String line) {
-    int maxJoltage = -1;
-    int highestFirst = line.charAt(0) - '0';
+private long maxJoltage(String line, int keep) {
+    int toRemove = line.length() - keep;
+    var stack = new StringBuilder();
 
-    for (int i = 1; i < line.length(); i++) {
-        int current = line.charAt(i) - '0';
-        maxJoltage = Math.max(maxJoltage, highestFirst * 10 + current);
-        highestFirst = Math.max(highestFirst, current);
+    for (char current : line.toCharArray()) {
+        while (toRemove > 0 && !stack.isEmpty() && stack.charAt(stack.length() - 1) < current) {
+            stack.deleteCharAt(stack.length() - 1);
+            toRemove--;
+        }
+        stack.append(current);
     }
-    return maxJoltage;
+
+    // In case we didn't remove enough (monotonic input)
+    return Long.parseLong(stack.substring(0, keep));
 }
