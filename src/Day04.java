e563c0764d2b109a1735359243c4293b07cb36d1
Cesar D. Garcia <cesardariogar@gmail.com>
2025-12-22 16:03:23 -0700

    complete Day04 Part II - working but can be improved

--- src/Day04.java
+++ src/Day04.java
@@ -1,33 +1,35 @@
 // static final String FILE_PATH = "resources/day04/printing_small.txt"; // Total = 13;
-static final String FILE_PATH = "resources/day04/printing.txt"; // Total = 1547
+static final String FILE_PATH = "resources/day04/printing.txt"; // Part I = Total = 1547, Part II = 8948
 final char PAPER_ROLL = '@';
 final int MAX_ADJACENT_ROLLS = 3;
 
 void main() throws IOException {
-    int availablePaperRollsTotal = 0;
+    List<String> linesList;
     try (var lines = Files.lines(Path.of(FILE_PATH))) {
-        List<String> linesList = lines
+        linesList = lines
                 .filter(Predicate.not(String::isEmpty))
-                .toList();
-        availablePaperRollsTotal = processMaze(linesList);
+                .collect(Collectors.toList());
     }
-    System.out.println("Total: " + availablePaperRollsTotal);
+
+    int rollsCanBeRemovedTotal = 0;
+    int currentlyRemoved = 0;
+    do {
+        rollsCanBeRemovedTotal += (currentlyRemoved = processMaze(linesList));
+    } while (currentlyRemoved != 0);
+    System.out.println("Total: " + rollsCanBeRemovedTotal);
 }
 
 private int processMaze(List<String> lines) {
     int blockAvailableRollsSum = 0;
     for (int l = 0; l < lines.size(); l++) {
-        String line = lines.get(l);
+        StringBuilder line = new StringBuilder(lines.get(l));
         for (int c = 0; c < line.length(); c++) {
-            char current = line.charAt(c);
-            if (isPaperRoll(current) == 0) {
-                continue;
-            } else {
-                int currentAdjacentRolls = 0;
+            if (isPaperRoll(line.charAt(c)) == 1) {
                 boolean hasBackColumn = (c > 0);
                 boolean hasFrontColumn = (c <= line.length() - 2);
                 boolean hasLineAbove = (l > 0);
                 boolean hasLineBelow = (l <= lines.size() - 2);
+                int currentAdjacentRolls = 0;
                 if (hasBackColumn) {
                     currentAdjacentRolls += isPaperRoll(line.charAt(c - 1));
                     if (hasLineAbove) {
@@ -54,6 +56,8 @@ private int processMaze(List<String> lines) {
                 }
                 if (currentAdjacentRolls <= MAX_ADJACENT_ROLLS) {
                     blockAvailableRollsSum++;
+                    line.setCharAt(c, '.');
+                    lines.set(l, line.toString());
                 }
             }
         }
