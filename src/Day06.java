b42e5beac0d4c5184af108f1421d5f496495e78b
Cesar D. Garcia <cesardariogar@gmail.com>
2025-12-24 15:59:34 -0700

    complete Day06 - Part II - class heavily refactored, still can be improved

--- src/Day06.java
+++ src/Day06.java
@@ -1,45 +1,98 @@
-final String FILE_PATH = "resources/Day06/trash_compactor.txt"; // total = 5733696195703
-// final String FILE_PATH = "resources/Day05/trash_compactor_small.txt"; // total = 4277556
+static final char MARKER = '@';
 
-static class Data {
-    Deque<Long> data = new ArrayDeque<>();
+private static class DataColumn {
+    List<Long> numbers = new ArrayList<>();
+    List<Long> cephalopodNumbers = new ArrayList<>();
+    List<String> rows = new ArrayList<>();
     String operand;
 
-    public Long processData() {
-        LongBinaryOperator op = operatorFrom(operand);
-        return data.stream().mapToLong(Long::longValue).reduce(op).orElseThrow();
+    private Long applyOperand(List<Long> numbers, String operand) {
+        LongBinaryOperator op = switch (operand) {
+            case "+" -> Long::sum;
+            case "*" -> (a, b) -> a * b;
+            default -> throw new IllegalArgumentException("Unknown operator: " + operand);
+        };
+        return numbers.stream().mapToLong(Long::longValue).reduce(op).orElseThrow();
     }
 }
 
-static LongBinaryOperator operatorFrom(String op) {
-    return switch (op) {
-        case "+" -> Long::sum;
-        case "*" -> (a, b) -> a * b;
-        case "-" -> (a, b) -> a - b;
-        case "/" -> (a, b) -> a / b;
-        default -> throw new IllegalArgumentException("Unknown operator: " + op);
-    };
+private List<Long> transposeRows(List<String> rows) {
+    int maxWidth = rows.stream()
+            .mapToInt(String::length)
+            .max()
+            .orElse(0);
+
+    List<Long> transposedNumbers = new ArrayList<>();
+    IntStream.range(0, maxWidth).forEach(col -> {
+        StringBuilder newNumber = new StringBuilder();
+        rows.forEach(currentRow -> {
+            char c = currentRow.charAt(col);
+            if (c != ' ') {newNumber.append(c);}
+        });
+        transposedNumbers.add(Long.parseLong(newNumber.toString()));
+    });
+
+    return transposedNumbers;
 }
+
 
 void main() throws IOException {
-    try (var input = Files.lines(Path.of(FILE_PATH))) {
-        List<String[]> lines = input
-                .filter(Predicate.not(String::isBlank))
-                .map(l -> l.trim().split("\\s+"))
-                .toList();
+    List<String> lines = getInput();
+    List<DataColumn> dataColumns = normalizeInput(lines);
 
-        List<Data> dataGroup = Stream.of(lines.getFirst()).map(col -> new Data()).toList();
-        for (int row = 0; row < lines.size(); row++) {
-            boolean isLastLine = row == lines.size() - 1;
-            for (int col = 0; col < lines.get(row).length; col++) {
-                if (isLastLine) {
-                    dataGroup.get(col).operand = lines.get(row)[col];
-                } else {
-                    dataGroup.get(col).data.push(Long.parseLong(lines.get(row)[col]));
-                }
-            }
-        }
-        Long total = dataGroup.stream().mapToLong(Data::processData).sum();
-        System.out.println(total);
-    }
+    // Part I
+    long numbersTotal = dataColumns.stream()
+            .mapToLong(column -> column.applyOperand(column.numbers, column.operand))
+            .sum();
+    System.out.println("Part I - total: " + numbersTotal);
+
+    // Part II
+    long transposedNumbersTotal = dataColumns.stream()
+            .mapToLong(column -> column.applyOperand(column.cephalopodNumbers, column.operand))
+            .sum();
+    System.out.println("Part II - total: " + transposedNumbersTotal);
+}
+
+
+private List<DataColumn> normalizeInput(List<String> lines) {
+    int[] columnIdx = getColumnsIdx(lines.getLast());
+    String[] operands = lines.getLast().trim().split("\\s+");
+
+    List<DataColumn> dataColumns = IntStream.range(0, columnIdx.length + 1)
+            .mapToObj(i -> new DataColumn())
+            .toList();
+
+    IntStream.range(0, lines.size() - 1).forEach(row -> {
+        String[] columns = getColumns(lines.get(row), columnIdx);
+        // Populate Data by Column
+        IntStream.range(0, columnIdx.length + 1).forEach(col -> {
+            dataColumns.get(col).rows.add(columns[col]);
+            dataColumns.get(col).numbers.add(Long.valueOf(columns[col].trim()));
+            dataColumns.get(col).operand = operands[col];
+        });
+    });
+
+    dataColumns.forEach(column -> column.cephalopodNumbers = transposeRows(column.rows));
+
+    return dataColumns;
+}
+
+private static String[] getColumns(String line, int[] columnIdx) {
+    StringBuilder sb = new StringBuilder(line);
+    Arrays.stream(columnIdx).forEach(idx -> sb.setCharAt(idx, MARKER));
+    return sb.toString().split(String.valueOf(MARKER));
+}
+
+private int[] getColumnsIdx(String operandsLine) {
+    return IntStream
+            .range(0, operandsLine.length() - 1)
+            .filter(i -> (i > 0) && (operandsLine.charAt(i + 1) != ' '))
+            .toArray();
+}
+
+private List<String> getInput() throws IOException {
+    String FILE_PATH = "resources/Day06/trash_compactor.txt";               // total = 5733696195703
+    //String FILE_PATH = "resources/Day05/trash_compactor_small.txt";       // total = 4277556
+
+    return Files.readAllLines(Path.of(FILE_PATH), StandardCharsets.UTF_8);
 }
